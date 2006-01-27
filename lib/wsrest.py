8347ad3bc0b763f6d58e23fdcf0b1106e2b49ebe
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2006-01-27 08:39:25 +0000

    * lib/wsrest.py Converted to use with mod_python style web services Changed status codes in exceptions from strings to ints Moved the functionality that was in __init__ to tripplanner/ui/web/webservices.py

--- lib/wsrest.py
+++ lib/wsrest.py
@@ -1,32 +1,41 @@
-import os, cgi, sys
+import os, sys
 
 
 class RestError(Exception): pass
     
 class BadRequestError(RestError):
     def __init__(self, reason='Bad Request'):
-        self.status = '400'
+        self.status = 400
         self.reason = reason
 
 class NotFoundError(RestError):
     def __init__(self, reason='Not Found'):
-        self.status = '404'
+        self.status = 404
         self.reason = reason
+
+class MultipleChoicesError(RestError):
+    def __init__(self, reason='Multiple Choices', choices=None):
+        self.status = 300
+        self.reason = reason
+        self.choices = choices
 
 class MethodNotAllowedError(RestError):
     def __init__(self):
-        self.status = '405'
+        self.status = 405
         self.reason = 'Method Not Allowed'
 
-    def getAllowHeader(self, restws):
+    def getAllowMethods(self, restws):
         allow = []
         for method, name in zip((restws.GET, restws.POST,
                                  restws.PUT, restws.DELETE),
                                 ("GET", "POST", "PUT", "DELETE")):
-            try: method()
-            except MethodNotAllowedError: pass
-            else: allow.append(name)
-        return "Allow: %s" % (", ".join(allow))
+            try:
+                method()
+            except MethodNotAllowedError:
+                pass
+            else:
+                allow.append(name)
+        return allow
 
 
 class ResultSet(object):
@@ -44,50 +53,18 @@ class ResultSet(object):
     
 
 class RestWebService(object):
-    def __init__(self):
-        try:
-            self.method = os.environ['REQUEST_METHOD']
-        except KeyError:
-            pass
-        else:
-            self.input = {}
-            input = cgi.FieldStorage()
-            for key in input:
-                self.input[key.strip()] = input.getfirst(key).strip()
+    def __init__(self, **params):
+        self.params = params
+        
+    def GET(self):
+        raise MethodNotAllowedError
 
-            self.status = '200'
-            self.reason = 'OK'
-            self.content = ''
-            self.content_length = 0
+    def POST(self):
+        raise MethodNotAllowedError
 
-            try:
-                self.content = eval("self." + self.method)()
-            except MethodNotAllowedError, e:
-                self.status = e.status
-                self.reason = e.reason + " (%s)" % self.method
-                self.headers += e.getAllowHeader(self)
-            except RestError, e:
-                self.status = e.status
-                self.reason = e.reason
-            except Exception, e:
-                self.status = '500'
-                self.reason = 'Internal Server Error (%s)' % str(e)
-                
-            if not self.content: self.content = self.reason
+    def PUT(self):
+        raise MethodNotAllowedError
 
-            if eval(repr(self.content)):
-                self.content_length = len(self.content)
-            self.headers = ["Status: %s %s" % (self.status,
-                                               self.reason),
-                            "Content-Type: text/plain",
-                            "Content-Length: %s" % self.content_length,
-                            "\r\n"]
-            sys.stdout.write("\r\n".join(self.headers))            
-            sys.stdout.flush()
-            if self.content_length: sys.stdout.write(self.content)
-
-    def GET(self): raise MethodNotAllowedError
-    def POST(self): raise MethodNotAllowedError
-    def PUT(self): raise MethodNotAllowedError
-    def DELETE(self): raise MethodNotAllowedError
+    def DELETE(self):
+        raise MethodNotAllowedError
 
