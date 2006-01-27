e5f3eedb557ac7819eb9559691c6a00553b3ed58
Wyatt <self@wyattbaldwin.com>
2006-01-27 08:46:50 +0000

    * tripplanner/services/route/__init__.py Moved print_key function inside 'if __name__ == __main__' so it will only be   defined during testing
    * tripplanner/ui/web/webservices.py
    Added for mod_python style web services
    
    * tripplanner/ui/web/tripplanner.tmpl
    Moved resizeMap function to ui.js
    Moved getWindowHeight function to util.js
    Moved inline script with call to resizeMap to window.onload handler (defined in
      ginit.js)
    
    * tripplanner/ui/web/index.py
    Modified for mod_python:
      Removed CGI stuff
      Made main functionality into a function (index)
      Added metro and milwaukee functions so the trip planner can be addressed as
        tp.bC.org/x where x is a region

---
+++ tripplanner/ui/web/webservices.py
@@ -0,0 +1,76 @@
+# Web Services Index
+
+def geocode(req, **params):
+    from byCycle.tripplanner.webservices import geocode
+    service = geocode.Geocode(**params)
+    return _process(req, service)
+
+
+def route(req, **params):
+    from byCycle.tripplanner.webservices import route
+    service = route.Route(**params)
+    return _process(req, service)
+
+
+def _process(req, service):
+    try: 
+        from mod_python import apache
+    except ImportError:
+        pass
+    from byCycle.lib import wsrest
+    
+    method = req.method
+    try:
+        content = eval('service.%s' % method)()
+    except wsrest.MethodNotAllowedError, exc:
+        reason = exc.reason + ' (%s)' % method
+        req.allow_methods(exc.getAllowMethods(self))
+    except wsrest.MultipleChoicesError, exc:
+        reason = exc.reason
+        content = exc.choices
+    except wsrest.RestError, exc:
+        reason = exc.reason
+    except Exception, exc:
+        status = 500
+        reason = 'Internal Server Error (%s)' % str(exc)
+    else:
+        status = 200
+        reason = 'OK'
+
+    try:
+        req.status = exc.status
+        req.reason = exc.reason
+    except (NameError, AttributeError):
+        req.status = status
+        req.reason = reason
+    
+    req.content_type = 'text/plain'
+    try:
+        return content
+    except NameError:
+        return ''
+
+
+if __name__ == '__main__':
+    class Req(object):
+        def __init__(self):
+            self.content_type = ''
+            self.headers_out = {}
+
+    def doRequest(service, **params):
+        req = Req()
+        req.method = 'GET'
+        content = service(req, **params)
+        print req.content_type
+        print req.headers_out
+        print req.status
+
+        #print req.reason
+        print content
+        print
+
+    #doRequest(geocode, dmode='milwaukee', q='124 and county line')
+    doRequest(route, dmode='milwaukee', tmode='bike',
+              q="['35th and north', '124 and county line']")
+    doRequest(route, dmode='milwaukee', tmode='bike',
+              q="['35th and north', '27th and lisbon']")
