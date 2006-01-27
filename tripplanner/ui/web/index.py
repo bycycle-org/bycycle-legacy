d3d3a2a208ba6e01016eced8980a7a5fb9377413
Wyatt <self@wyattbaldwin.com>
2006-01-27 11:49:40 +0000

    * index.py Removed region functions because region will now be passed as a CGI param Changed the region style to Area State Added facilities to display selected region in template output
    * tripplanner.tmpl
    Added facilities to display selected region

--- tripplanner/ui/web/index.py
+++ tripplanner/ui/web/index.py
@@ -1,47 +1,49 @@
-def milwaukee(**params):
-    return index(region='milwaukee', **params)
-
-
-def metro(**params):
-    return index(region='metro', **params)
-portland = metro
-
-
-def index(region='', **params):
+def index(region=''):
     import os, datetime
     import byCycle
-    
-    template = '%stripplanner/ui/web/tripplanner.tmpl' % byCycle.install_path
+
+    region = region.lower().replace(',', '')
 
     # Create an options list of regions, setting the selected region if we got
     # here by http://tripplanner.bycycle.org/x where x is the name of some
     # region
     region_option = '<option value="%s">%s</option>'
     region_option_selected = '<option value="%s" selected="selected">%s</option>'
-    regions_map = {'Milwaukee, WI': 'milwaukee',
-                  'Portland, OR': 'metro'}
-    regions = regions_map.keys()
-    regions.sort()
+    regions = {'wi': ['milwaukee',
+                      ],
+               'or': ['portland',
+                      ],
+               }
+    states = regions.keys()
+    states.sort()
     regions_option_list = []
-    for r in regions:
-        region_abbrev = regions_map[r]
-        if region_abbrev == region:
-            regions_option_list.append(region_option_selected%(region_abbrev, r))
-        else:
-            regions_option_list.append(region_option%(region_abbrev, r))
+    region_display = ''
+    for state in states:
+        areas = regions[state]
+        #areas.sort()
+        for area in areas:
+            reg = '%s%s' % (area, state)
+            if reg == region:
+                opt = region_option_selected
+                region_display = ' - %s, %s' % (area.title(), state.upper())
+            else:
+                opt = region_option 
+            regions_option_list.append(opt % (reg, '%s, %s' % (area.title(), state.upper())))
     regions_option_list = '\n'.join(regions_option_list)
 
-    # Get the last modified date of the template
+    template = '%stripplanner/ui/web/tripplanner.tmpl' % byCycle.install_path
+
+    # Get and format the last modified date of the template
     stat = os.stat(template)
     last_modified = datetime.date.fromtimestamp(stat.st_mtime)
     last_modified = last_modified.strftime("%B %d, %Y")
 
-
-    data = {'last_modified': last_modified,
-            'regions_option_list': regions_option_list}
-
     template_file = open(template)
-    result = template_file.read() % data
+    data = {'last_modified': last_modified,
+            'regions_option_list': regions_option_list,
+            'region': region_display,
+            }
+    content = template_file.read() % data
     template_file.close()
-    return result
+    return content
 
