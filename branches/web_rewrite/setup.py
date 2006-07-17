056fc19028166ff6be3ce6df3385009c1ba5d715
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2006-07-17 10:33:00 +0000

    Created branch for rewrite of web interface. The rewrite will include converting all JavaScript to being object/module/namespace-oriented and making the UI more useable & less cluttered.

---
+++ branches/web_rewrite/setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+import sys
+
+if sys.version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+    
+setup(name='byCycle',
+      description='byCycle Trip Planner',
+      long_description='byCycle Trip Planner',
+      author='Wyatt L. Baldwin',
+      author_email='wyatt@byCycle.org',
+      url='http://www.byCycle.org/',
+      version='0.3alpha',
+      classifiers=['Development Status - Alpha',
+                   'License :: GPL for noncommercial use only',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'Environment :: Console',
+                   'Environment :: Web Environment',
+                   ],
+      packages=['lib',
+                'tripplanner',
+                'tripplanner.model',
+                'tripplanner.model.tests',
+                'tripplanner.model.milwaukeewi',
+                'tripplanner.model.milwaukeewi.data',
+                'tripplanner.model.portlandor',
+                'tripplanner.model.portlandor.data',
+                'tripplanner.model.pittsburghpa',
+                'tripplanner.model.pittsburghpa.data',
+                'tripplanner.services',
+                'tripplanner.services.geocode',                
+                'tripplanner.services.route',
+                'tripplanner.ui',
+                'tripplanner.ui.web',
+                'tripplanner.webservices',
+                ],
+      )
+
