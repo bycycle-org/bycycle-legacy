53ad3575812fa6103d47349e8b7e849bac5ba787
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2006-08-15 18:05:54 +0000

    * __init__.py Added a license notice with the SVN Id keyword.

--- branches/web_rewrite/__init__.py
+++ branches/web_rewrite/__init__.py
@@ -1,1 +1,10 @@
-install_path = '/home/u6/bycycle/byCycle/evil/byCycle'
+"""$Id$
+
+Top level of the byCycle library.
+
+Copyright (C) 2006 Wyatt Baldwin <wyatt@bycycle.org>
+
+This file doesn't need a license.
+
+"""
+install_path = '/home/bycycle/byCycle/evil/byCycle'
