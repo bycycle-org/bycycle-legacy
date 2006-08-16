1601feae72eb421a7cecb723ca0263e372b29410
Wyatt <self@wyattbaldwin.com>
2006-08-16 01:26:11 +0000

    * Added license notice to all files.
    * scripts/test_bycycle.py
    Added module for testing of bycycle script.

--- branches/web_rewrite/scripts/test_bycycle.py
+++ branches/web_rewrite/scripts/test_bycycle.py
@@ -30,30 +30,12 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
-# Milwaukee, WI Data Mode
-# 11/07/2005
-
-from byCycle.tripplanner.model import mode
- 
-
-class Mode(mode.Mode):
-    def __init__(self):
-        self.region = 'milwaukeewi'
-        self.edge_attrs = ['bikemode', 'lanes', 'adt', 'spd']
-        mode.Mode.__init__(self)
-
-    def _adjustRowForMatrix(self, row):
-        one_way = row['one_way']
-        if one_way == 'ft':
-            one_way = 1
-        elif one_way == 'tf':
-            one_way = 2
-        elif one_way == '':
-            one_way = 3
-        else:
-            one_way = 0
-        row['one_way'] = one_way
-
+import bycycle
 if __name__ == '__main__':
-    md = Mode();
-    G = md.createAdjacencyMatrix()
+    args_list = (
+        '; n; 633 n alberta; portlandor',
+        '; n; n',
+    )
+    for args in args_list:
+        argv = [a.strip() for a in args.split(';')]
+        bycycle.main(argv)
