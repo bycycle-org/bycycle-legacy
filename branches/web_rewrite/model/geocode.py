66fe5d285a98844ad9955fb64552e0e36e6b7dcc
Wyatt <self@wyattbaldwin.com>
2006-09-11 04:16:40 +0000

    M    model/geocode.py - Renamed lon_lat to xy - Removed edge_id and node_id from __repr__s and replaced with network_id
    M    model/mode.py
    - Renamed lon_lat to xy
    - Removed tripplanner from import paths
    - New license
    - Switched back (temporarily) to travel modes (bicycle.py) being subclasses
      of mode.Mode

--- branches/web_rewrite/model/geocode.py
+++ branches/web_rewrite/model/geocode.py
@@ -1,38 +1,13 @@
 """$Id$
 
-Description goes here.
+Geocode classes.
 
-Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>
-
-All rights reserved.
-
-TERMS AND CONDITIONS FOR USE, MODIFICATION, DISTRIBUTION
-
-1. The software may be used and modified by individuals for noncommercial, 
-private use.
-
-2. The software may not be used for any commercial purpose.
-
-3. The software may not be made available as a service to the public or within 
-any organization.
-
-4. The software may not be redistributed.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>. All rights 
+reserved. Please see the LICENSE file included in the distribution. The license 
+is also available online at http://bycycle.org/tripplanner/license.txt or by 
+writing to license@bycycle.org.
 
 """
-# Geocode classes
-
-
 from byCycle.lib import gis
 
     
@@ -45,17 +20,20 @@ class PostalGeocode(Geocode):
     def __init__(self, address=None, segment=None, xy=None):
         self.address = address
         self.segment = segment
+        self.network_id = segment.id
         self.xy = xy
         
     def __repr__(self):
-        result = {'type': 'postal',
-                  'number': self.address.number,
-                  'street': self.address.street,
-                  'place': self.address.place,
-                  'address': str(self.address),
-                  'x': '%.6f' % self.xy.x,
-                  'y': '%.6f' % self.xy.y,
-                  'edge_id': self.segment.id}
+        result = {
+            'type': 'postal',
+            'number': self.address.number,
+            'street': self.address.street,
+            'place': self.address.place,
+            'address': str(self.address),
+            'x': '%.6f' % self.xy.x,
+            'y': '%.6f' % self.xy.y,
+            'network_id': self.network_id
+        }
         return repr(result)
 
     
@@ -63,16 +41,19 @@ class IntersectionGeocode(Geocode):
     def __init__(self, address=None, intersection=None):
         self.address = address
         self.intersection = intersection
-        self.xy = intersection.lon_lat
+        self.network_id = intersection.id
+        self.xy = intersection.xy
 
     def __repr__(self):
-        result = {'type': 'intersection',
-                  'street1': self.address.street1,
-                  'street2': self.address.street2,
-                  'place1': self.address.place1,
-                  'place2': self.address.place2,
-                  'address': str(self.address),                  
-                  'x': '%.6f' % self.xy.x,
-                  'y': '%.6f' % self.xy.y,
-                  'node_id': self.intersection.id}
+        result = {
+            'type': 'intersection',
+            'street1': self.address.street1,
+            'street2': self.address.street2,
+            'place1': self.address.place1,
+            'place2': self.address.place2,
+            'address': str(self.address),                  
+            'x': '%.6f' % self.xy.x,
+            'y': '%.6f' % self.xy.y,
+            'network_id': self.network_id
+        }
         return repr(result)
