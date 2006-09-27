495fce6718ef859232dcba87f6d58188466d7843
Wyatt <self@wyattbaldwin.com>
2006-09-27 08:56:50 +0000

    M    excs.py In __str__ methods, call string on description or errors, in case they're not already for some odd reason.

--- core/branches/enterprise/services/excs.py
+++ core/branches/enterprise/services/excs.py
@@ -1,24 +1,25 @@
-"""$Id$
-
-Description goes here.
-
-Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>. All rights 
-reserved. Please see the LICENSE file included in the distribution. The license 
-is also available online at http://bycycle.org/tripplanner/license.txt or by 
-writing to license@bycycle.org.
-
-"""
+###########################################################################
+# $Id$
+# Created 2005-??-??.
+#
+# byCycle Exceptions.
+#
+# Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>.
+# All rights reserved.
+#
+# For terms of use and warranty details, please see the LICENSE file included
+# in the top level of this distribution. This software is provided AS IS with
+# NO WARRANTY OF ANY KIND.
 
 
 class ByCycleError(Exception):
     def __init__(self, desc='byCycle Error'): 
         self.description = desc
-        
     def __str__(self):
-        return self.description
+        return str(self.description)
 
         
 class InputError(ByCycleError):
     def __init__(self, errors=[]):
-        desc = '\n'.join(errors)
+        desc = '\n'.join([str(e) for e in errors])
         ByCycleError.__init__(self, desc=desc)
