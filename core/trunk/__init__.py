1ba87ef7594286fbcf3c022b04bd9880464603e2
Wyatt <self@wyattbaldwin.com>
2006-10-26 10:59:05 +0000

    Changed path to use /home/u2 instead of /home/u6 since aplus changed our host and home dir without notification.

--- core/trunk/__init__.py
+++ core/trunk/__init__.py
@@ -30,4 +30,5 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
-install_path = '/home/u6/bycycle/lib/python2.4/site-packages/byCycle'
+install_path = '/home/u2/bycycle/lib/python2.4/site-packages/byCycle'
+
