25dbbcc32d7c1445d695e99eaced5736ed22853c
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2006-10-27 11:18:20 +0000

    Updated after installing new version of PCL (r550)
    * enterprise/model/portlandor/data/shp2pgsql.py
    - Changed path to PostGIS shp2pgsql executable to /usr/bin
    - (vacuum): Accept *args and if args only vacuum the indicated tables instead
      of all tables
    - (createTables): Create region schemas if they don't exist
    
    * enterprise/model/data/sqltypes.py
    - Changed import path to PCL geometry package and SpatialReference class
    - Changed old-style calls to __init__ to use super
    - (Geometry): Fixed convert_bind_param
    - (Geometry::convert_bind_param): Added SRID=X to stop SRID constraint error
    - (transferNodeIDs): Just copy the node IDs; don't insert NULL geometries
    - (transferNodeGeometry): Select complete linestring from edges and use PCL
      to get start and end points (doing a direct start/endpoint on the table
      gets a hex string that can't be inserted into the nodes table by sqlalchemy,
      because it's expecting a PCL geometry)
    
    * enterprise/services/route/__init__.py
    Changed import path to PCL geometry package

--- core/branches/enterprise/model/data/sqltypes.py
+++ core/branches/enterprise/model/data/sqltypes.py
@@ -1,79 +1,83 @@
 import sqlalchemy
-from cartography.spatial import geometry
-from cartography.referencing.srs import SpatialReference
+from cartography import geometry
+from cartography.proj import SpatialReference
 from binascii import a2b_hex, b2a_hex
 
 
 class Geometry(sqlalchemy.types.TypeEngine):
     """PostGIS Geometry Type."""
-    
+
     def __init__(self, SRID, type_, dimension):
         """
-        
+
         ``SRID`` `int` -- Spatial Reference ID
         ``type_`` `string` -- Geometry type (POINT, LINESTRING, etc)
         ``dimension`` `int` -- Geometry dimensions (2D, 3D, etc)
-        
+
         From PostGIS documentation:
-        
+
         AddGeometryColumn(
             <schema_name>, <table_name>, <column_name>, <SRID>, <type>,
             <dimension>
         )
-        
+
         Example:
-        
-        AddGeometryColumn(        
+
+        AddGeometryColumn(
             'portlandor', 'layer_edges', 'geom', 2913, 'MULTILINESTRING', 2
         )
-        
+
         First three params are given; we need to get the last three from the
         users of this class.
-        
+
         """
+        super(Geometry, self).__init__()
         self.SRID = SRID
         self.srs = SpatialReference(epsg=SRID)
         self.type = type_.upper()
         self.dimension = dimension
-        
+
     def get_col_spec(self):
         """What exactly is this supposed to return?
-        
+
         I *think* it's what goes in the table definition. If so, I think this
         is correct. AFAICT, PostGIS geometry types are defined by constraints.
-        
+
         """
         return 'GEOMETRY'
-    
+
     def convert_bind_param(self, value, engine):
         """Convert value from Python ==> database.
-        
-        ``value`` -- A geometry object of some kind. The database expects a 
+
+        ``value`` -- A geometry object of some kind. The database expects a
         hex string representing a WKB geometry.
-        
+
         """
-        return b2a_hex(value.toWKB())
-                
+        #v = super(Geometry, self).convert_bind_param(v, engine)
+        return 'SRID=%s;%s' % (self.SRID, b2a_hex(value.toWKB()))
+
     def convert_result_value(self, value, engine):
         """Convert ``value`` from database ==> Python.
 
         ``value`` `string` -- PostGIS geometry value from the database. It is
         a hex string representing a WKB geometry.
-        
+
         """
+        #v = super(Geometry, self).convert_result_value(value, engine)
         g = geometry.Geometry.fromWKB(a2b_hex(value), srs=self.srs)
         return g
 
 
 class MULTILINESTRING(Geometry):
     def __init__(self, SRID):
-        Geometry.__init__(self, SRID, 'MULTILINESTRING', 2)
+        super(MULTILINESTRING, self).__init__(SRID, 'MULTILINESTRING', 2)
+
 
 class LINESTRING(Geometry):
     def __init__(self, SRID):
-        Geometry.__init__(self, SRID, 'LINESTRING', 2)
+        super(LINESTRING, self).__init__(SRID, 'LINESTRING', 2)
+
 
 class POINT(Geometry):
     def __init__(self, SRID):
-        Geometry.__init__(self, SRID, 'POINT', 2)
-
+        super(POINT, self).__init__(SRID, 'POINT', 2)
