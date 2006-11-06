c6cfb8e99a8e78b022e361883672f67ea62d1309
Wyatt <self@wyattbaldwin.com>
2006-11-06 14:29:25 +0000

    Changed Timer.__init__ keyword arg start_now default to False; was True, which was a bug.

--- core/branches/enterprise/lib/meter.py
+++ core/branches/enterprise/lib/meter.py
@@ -1,36 +1,6 @@
-"""$Id$
-
-Description goes here.
-
-Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>
-
-All rights reserved.
-
-TERMS AND CONDITIONS FOR USE, MODIFICATION, DISTRIBUTION
-
-1. The software may be used and modified by individuals for noncommercial, 
-private use.
-
-2. The software may not be used for any commercial purpose.
-
-3. The software may not be made available as a service to the public or within 
-any organization.
-
-4. The software may not be redistributed.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-"""
+# $Id$
 import sys, time
+
 
 class Meter(object):
     def __init__(self, percentages=None, num_items=0, start_now=False):
@@ -97,7 +67,7 @@ class Meter(object):
         return False
 
     def printElapsedTime(self, last_item_number):
-        """Print elapsed number of seconds since the timer was started (if it was)."""
+        """Print number of seconds since the timer was started (if it was)."""
         if self.start_time:
             elapsed_time = int(round(time.time() - self.start_time))
             units = "seconds"
@@ -123,7 +93,7 @@ class Meter(object):
 class Timer(object):
     """Super simple wall clock timer."""
     
-    def __init__(self, start_now=True):
+    def __init__(self, start_now=False):
         """Create a new `Timer`, usually not started until `start` is called.
         
         ``start_now`` -- If set, start now instead of waiting for `start` to
