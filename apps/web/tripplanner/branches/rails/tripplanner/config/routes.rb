8b74dc34e3ecddfbaa014877500da76bc3adec1d
Wyatt <self@wyattbaldwin.com>
2007-01-21 20:36:58 +0000

    * Ignore tmp/ at top level * Set default route to regions/index * Renamed doc/README_FOR_APP to README

--- apps/web/tripplanner/branches/rails/tripplanner/config/routes.rb
+++ apps/web/tripplanner/branches/rails/tripplanner/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :regions
+
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Sample of regular route:
@@ -11,7 +13,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  # map.connect '', :controller => "welcome"
+  map.connect '', :controller => 'regions'
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
