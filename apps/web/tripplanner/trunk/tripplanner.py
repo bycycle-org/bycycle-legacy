df7115186ab24046d0f063bb2e977ac18e52b584
Wyatt <self@wyattbaldwin.com>
2007-01-25 08:36:30 +0000

    * trunk/tripplanner.py Added $$$ select box to params so it can be reused in geocode and route forms
    * trunk/static/css/base.css
    Changed width of #fr and #to so the route form is the same width as the
    geocode form
    
    * trunk/static/css/base_ie.css
    # Removed unused rules (all of them except the VML thing)
    
    * trunk/static/js/ui.js
    (doFind): Check if there's a $$$ amount selected and pop open donate window
    if there is
    
    * trunk/templates/tripplanner.html
    Removed duplicated $$$ select box and insert it from tripplanner.render()
    for both geocode and route forms

--- apps/web/tripplanner/trunk/tripplanner.py
+++ apps/web/tripplanner/trunk/tripplanner.py
@@ -267,6 +267,25 @@ class TripPlanner(object):
             params['response_text'] = urllib.quote(simplejson.dumps(result_set))
             params['regions_opt_list'] = self.makeRegionsOptionList(**params)
             params['result'] = result
+            params['amount_options'] = """
+            <option value="">[$$$]</option>
+            <option value="2">$2</option>
+            <option value="5">$5</option>
+            <option value="10">$10</option>
+            <option value="15">$15</option>
+            <option value="20">$20</option>
+            <option value="25">$25</option>
+            <option value="30">$30</option>
+            <option value="35">$35</option>
+            <option value="40">$40</option>
+            <option value="45">$45</option>
+            <option value="50">$50</option>
+            <option value="75">$75</option>
+            <option value="100">$100</option>
+            <option value="250">$250</option>
+            <option value="500">$500</option>
+            <option value="1000">$1000</option>
+            <option value="other">Other</option>"""
 
             for p in params:
                 if params[p] is None:
