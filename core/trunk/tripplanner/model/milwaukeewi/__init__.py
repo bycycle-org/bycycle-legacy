a2ea8d9b382f6a817de00c8c0d8213c240349cf6
Wyatt <self@wyattbaldwin.com>
2007-02-12 03:44:26 +0000

    * tripplanner/model/portlandor/__init__.py   * Removed setting of sys.path at top before import of bycycle package (not     sure why that was there, but it was causing import from wrong location)
    * tripplanner/model/milwaukeewi/__init__.py
    * tripplanner/model/pittsburghpa/__init__.py
    Moved some comments up into the license header

--- core/trunk/tripplanner/model/milwaukeewi/__init__.py
+++ core/trunk/tripplanner/model/milwaukeewi/__init__.py
@@ -1,6 +1,7 @@
 """$Id$
 
-Description goes here.
+Milwaukee, WI Data Mode
+Created 11/07/2005
 
 Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>
 
@@ -30,9 +31,6 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
-# Milwaukee, WI Data Mode
-# 11/07/2005
-
 from byCycle.tripplanner.model import mode
  
 
