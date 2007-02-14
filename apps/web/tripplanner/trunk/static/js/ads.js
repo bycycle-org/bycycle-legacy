27dee4a44414663d1a2abe1fa49ab489c944b04d
Wyatt <self@wyattbaldwin.com>
2007-02-14 23:24:27 +0000

    ads.js -- Removed from template to reduce clutter

---
+++ apps/web/tripplanner/trunk/static/js/ads.js
@@ -0,0 +1,12 @@
+google_ad_client = "pub-6971619329897557";
+google_ad_width = 120;
+google_ad_height = 600;
+google_ad_format = "120x600_as";
+google_ad_type = "text_image";
+google_ad_channel ="";
+google_color_border = "A7CC95";
+google_color_bg = "FFFFFF";
+google_color_link = "000000";
+google_color_text = "000000";
+google_color_url = "4466DD";
+script("http://pagead2.googlesyndication.com/pagead/show_ads.js");
