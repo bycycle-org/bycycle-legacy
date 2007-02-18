96debd38b9b46a1d593ff2c54402d8a9ed1299ed
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2007-02-18 01:14:44 +0000

    * db.py   * Updated docstring   * Removed ``region`` arg to constructor (unused and unwanted--DB should be     region-agnostic)   * Removed self.region and self.schema (same as above)   * Added self.model_path (stole it from region.py)   * Added return doc to createConnection() docstring
    * region.py
      * Removed self.model_path to db.DB since it's needed there and here and is
        common to all regions
      * Get DB, then set up data_path and matrix_path

--- core/branches/enterprise/byCycle/model/db.py
+++ core/branches/enterprise/byCycle/model/db.py
@@ -2,7 +2,7 @@
 # $Id$
 # Created 2005-11-07.
 #
-# Database Abstraction Layer.
+# Database Connection Handler.
 #
 # Copyright (C) 2006 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>.
 # All rights reserved.
@@ -11,16 +11,14 @@
 # in the top level of this distribution. This software is provided AS IS with
 # NO WARRANTY OF ANY KIND.
 ################################################################################
-"""Database initialization and handling.
+"""Database connection initialization and handling.
 
-TODO:
-  - Use fancy-pants ORM (SQLAlchemy, for example) to create proper model/domain
-    objects.
-  - Decouple regions from this class (i.e., they shouldn't be subclasses)--when
-    moving to using an ORM, move the region-common stuff into a Region base
-    class.
-  - Make createAdjacencyMatrix available through bycycle script.
-  - Should DB class be a Singleton (per region)???
+Provides the ``DB`` class, which connects to the database and contains various
+generic (i.e., not region-specific) database functions.
+
+TODO: The ``DB`` class is a (poorly implemented) Singleton. Ian Bicking thinks
+Singletons are stupid. At the very least, we should use a better
+implementation.
 
 """
 import os
@@ -35,16 +33,11 @@ from sqlalchemy.orm import create_session
 
 
 class _DB(object):
-    """A database handler for a specific region."""
+    """Global database connection handler."""
 
-    def __init__(self, region):
-        """Create a database handler for ``region``.
-
-        ``region`` `Region`
-
-        """
-        self.region = region
-        self.schema = region.key
+    def __init__(self):
+        """Create the global (Singleton) database handler."""
+        self.model_path = os.path.abspath(os.path.dirname(__file__))        
         self.engine = create_engine('postgres://', creator=self.createConnection)
         self.metadata = BoundMetaData(self.engine)
         self.raw_metadata = BoundMetaData(self.engine)
@@ -52,7 +45,7 @@ class _DB(object):
 
     def createConnection(self):
         """Set up and return underlying DB connection."""
-        pw_path = os.path.join(self.region.model_path, '.pw')
+        pw_path = os.path.join(self.model_path, '.pw')
         pw_file = file(pw_path)
         pw = pw_file.read().strip()
         pw_file.close()
@@ -68,6 +61,12 @@ class _DB(object):
         ``ids`` One or more row IDs
         ``mapper`` DB to object mapper
         ``table`` Table to fetch from
+        
+        return `list`
+          A list of domain objects corresponding to the IDs passed via ``ids``.
+          Any ID in ``ids`` that doesn't correspond to a row in ``table`` will
+          be ignored (for now), so the list may not contain the same number of
+          objects as len(ids). If ``ids`` is empty, an empty list is returned.
 
         """
         query = self.session.query(mapper)
@@ -78,7 +77,8 @@ class _DB(object):
             try:
                 ordered_objects.append(objects_by_id[i])
             except KeyError:
-                # TODO: Should we insert None instead???
+                # No row with ID==i in ``table``
+                # TODO: Should we insert None instead or raise???
                 pass
         return ordered_objects
 
