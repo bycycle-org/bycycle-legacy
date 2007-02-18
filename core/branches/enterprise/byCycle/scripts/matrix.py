f1665addd8d2524da140ce6aec4b1a5286dea16d
Wyatt <self@wyattbaldwin.com>
2007-02-18 01:57:27 +0000

    Accept region key/name or "all" on command line; catch errors; Added die function; Added make_matrix_for_region function

--- core/branches/enterprise/byCycle/scripts/matrix.py
+++ core/branches/enterprise/byCycle/scripts/matrix.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
+"""Create adjacency matrix for region given as first arg on command line."""
+import sys
+from byCycle.model import regions
 
-from byCycle.model import portlandor
-region = portlandor.Region()
-region.createAdjacencyMatrix()
+no_region_msg = ('Specify a region name. Use "all" to create matrices for '
+                 'all regions.')
+
+def die(error_msg):
+    sys.stderr.write(str(error_msg) + '\n')
+    sys.exit(1)
+
+region_key = sys.argv[1] if len(sys.argv) > 1 else die(no_region_msg)
+
+def make_matrix_for_region(region_key):
+    try:
+        region = regions.getRegion(region_key)
+    except ValueError, e:
+        die('Unknown region: %s.' % region_key)
+    else:
+        region.createAdjacencyMatrix()
+
+if region_key == 'all':
+    for key in regions.region_keys:
+        make_matrix_for_region(key)
+else:
+    make_matrix_for_region(region_key)
 
