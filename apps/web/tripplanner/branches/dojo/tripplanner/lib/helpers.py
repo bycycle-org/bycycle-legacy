0cbc317d466d6c6c5c4fae94d2a9362f6e6897ac
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2007-02-19 23:51:49 +0000

    Added if_ie helper that just wraps some content in <!--[if IE]><[endif]-->

--- apps/web/tripplanner/branches/dojo/tripplanner/lib/helpers.py
+++ apps/web/tripplanner/branches/dojo/tripplanner/lib/helpers.py
@@ -6,3 +6,7 @@ All names available in this module will be available under the Pylons h object.
 from webhelpers import *
 from pylons.helpers import log
 from pylons.i18n import get_lang, set_lang
+
+
+def if_ie(content, join_string=''):
+    return join_string.join(('<!--[if IE]>', content, '<![endif]-->'))
