4103da23fc8e70f8a49c75ca9787505ec23acb7a
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2007-02-19 10:28:22 +0000

    routing.py -- Added route for /:region for testing Mako changes; need to add RESTful routes

--- apps/web/tripplanner/branches/dojo/tripplanner/config/routing.py
+++ apps/web/tripplanner/branches/dojo/tripplanner/config/routing.py
@@ -1,23 +1,31 @@
-"""
-Setup your Routes options here
-"""
 import os
 from routes import Mapper
 
+
 def make_map(global_conf={}, app_conf={}):
     root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    map_ = Mapper(directory=os.path.join(root_path, 'controllers'))
 
-    map = Mapper(directory=os.path.join(root_path, 'controllers'))
-    
-    # This route handles displaying the error page and graphics used in the 404/500
-    # error pages. It should likely stay at the top to ensure that the error page is
-    # displayed properly.
-    map.connect('error/:action/:id', controller='error')
-    
-    # Define your routes. The more specific and detailed routes should be defined first,
-    # so they may take precedent over the more generic routes. For more information, refer
-    # to the routes manual @ http://routes.groovie.org/docs/
-    map.connect(':controller/:action/:id')
-    map.connect('*url', controller='template', action='view')
+    # This route handles displaying the error page and graphics used in the
+    # 404/500 error pages. It should likely stay at the top to ensure that the
+    # error page is displayed properly.
+    map_.connect('error/:action/:id', controller='error')
 
-    return map
+    map_.connect(
+        ':region',
+        controller='region',
+        action='show',
+        conditions=dict(method=['GET']),
+    )
+    
+    # Define your routes. The more specific and detailed routes should be
+    # defined first, so they may take precedent over the more generic routes.
+    # For more information, refer to the routes manual @
+    # http://routes.groovie.org/docs/
+
+    # Default routes
+    map_.connect(':controller/:action/:id')
+    # This one can be used to display a template directly
+    map_.connect('*url', controller='template', action='view')
+
+    return map_
