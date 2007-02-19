8dadd9f27bfa5025fc6413b83eeb9f9766b075b3
Wyatt <self@wyattbaldwin.com>
2007-02-19 10:00:09 +0000

    * regions.js   * Removed pittsburghpa   * Removed some old cruft   * Removed 'all' as a region and added a bounds attr to byCycyle.UI.regions   * Moved milwaukeewi and portlandor objects into new regions attr     (e.g., byCycle.UI.regions.portlandor =>            byCycle.UI.regions.regions.portlandor).   * Dynamically calculate bounds containing all regions

--- apps/web/tripplanner/branches/dojo/tripplanner/public/javascripts/regions.js
+++ apps/web/tripplanner/branches/dojo/tripplanner/public/javascripts/regions.js
@@ -1,70 +1,44 @@
-byCycle.regions = {
-  all: {
-    id: 'all',
-    all: true,
-    heading: 'All',
-    subheading: 'Welcome to the <a href="http://www.bycycle.org/" title="byCycle Home Page">byCycle</a> interactive bicycle trip planner, version 0.3.5 beta.',
-    bounds: {
-      sw: {
-        x: -123.485755,
-        y: 40.362070
-      },
-      ne: {
-        x: -79.866439,
-        y: 45.814153
+byCycle.regions = (function() {
+  // sw => minx, miny
+  // ne => maxx, maxy
+  // Initially set to sw => max_possible, ne => min_possible
+  var bounds = {sw: {x: 180, y: 90}, ne: {x: -180, y: -90}};
+
+  var regions = {
+    portlandor: {
+      bounds: {
+        sw: {x: -123.485755, y: 44.885219},
+        ne: {x: -121.649618, y: 45.814153}
+      }
+    },
+
+    milwaukeewi: {
+      bounds: {
+        sw: {x: -88.069888, y: 42.842059},
+        ne: {x: -87.828241, y: 43.192647}
       }
     }
-  },
+  };
 
-  portlandor: {
-    id: 'portlandor',
-    heading: 'Portland, OR',
-    subheading: 'Developed in cooperation with <a href="http://www.metro-region.org/">Metro</a>.',
-    bounds: {
-      sw: {
-        x: -123.485755,
-        y: 44.885219
-      },
-      ne: {
-        x: -121.649618,
-        y: 45.814153
-      }
+  // Calculate minimum bounds containing all regions
+  for (var key in regions) {
+    var r = regions[key];
+    if (r.bounds.sw.x < bounds.sw.x) {
+      bounds.sw.x = r.bounds.sw.x;
     }
-  },
-
-  milwaukeewi: {
-    id: 'milwaukeewi',
-    heading: 'Milwaukee, WI',
-    subheading: 'Developed in cooperation with the <a href="http://www.bfw.org/">Bicycle Federation of Wisconsin</a>.',
-    img_src: 'static/images/bfw_logo.gif',
-    img_width: 70,
-    img_height: 71,
-    href: 'http://www.bfw.org/',
-    bounds: {
-      sw: {
-        x: -88.069888,
-        y: 42.842059
-      },
-      ne: {
-        x: -87.828241,
-        y: 43.192647
-      }
+    if (r.bounds.sw.y < bounds.sw.y) {
+      bounds.sw.y = r.bounds.sw.y;
     }
-  }
+    if (r.bounds.ne.x > bounds.ne.x) {
+      bounds.ne.x = r.bounds.ne.x;
+    }
+    if (r.bounds.ne.y > bounds.ne.y) {
+      bounds.ne.y = r.bounds.ne.y;
+    }
+  };
 
-  //pittsburghpa: {
-    //id: 'pittsburghpa',
-    //heading: 'Pittsburgh, PA',
-    //subheading: 'Developed in cooperation with Jessi.',
-    //bounds: {
-      //sw: {
-        //x: -80.088957,
-        //y: 40.362070
-      //},
-      //ne: {
-        //x: -79.866439,
-        //y: 40.500887
-      //}
-    //}
-  //}
-};
+  return {
+    bounds: bounds,
+    regions: regions
+  };
+})();
