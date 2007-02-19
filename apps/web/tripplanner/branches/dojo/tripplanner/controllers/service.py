6cbf5d7e1cdbbefbc339477ba87ee5cf760c4c80
Wyatt <self@wyattbaldwin.com>
2007-02-19 09:52:15 +0000

    * service.py   * (_get_response)     * Added ``wrapped_in`` arg     * Removed arg docs from docstring for old args (service, template, format,       http_status, message, results)     * Get content and mimetype from specific _get_<format>_response methods       and return the Response, instead of having each method individually       return its own response   * (_wrap_response_in): Added to wrap response in template if ``wrapped_in``     template specified to _get_response (does what _get_html_response used     to do)   * (_get_text_response, _get_json_response, _get_html_response): Return     content and mimetype to _get_response instead of returning Responses     individually

--- apps/web/tripplanner/branches/dojo/tripplanner/controllers/service.py
+++ apps/web/tripplanner/branches/dojo/tripplanner/controllers/service.py
@@ -14,7 +14,7 @@ class ServiceController(RestController):
     def show(self, query, region, service_class=None, **params):
         """Show the result of ``query``ing a service.
 
-        Subclasses should call return this method. In other words, they should
+        Subclasses should return this method. In other words, they should
         call this and return the result... unless a service-specific
         ByCycleError is encountered, in which case control should return to
         the subclass to handle the error and render the result.
@@ -82,63 +82,55 @@ class ServiceController(RestController):
         return self._get_response()
 
     #----------------------------------------------------------------------
-    def _get_response(self):
+    def _get_response(self, wrapped_in=None):
         """Render response after query to service.
 
         Return the appropriate type of response according to ``format``.
-        ``format`` may be one of "frag" (or "fragment"), "json", or "html"
-        (default). "json" will return a JSON object. "html" will return a
-        complete HTML template with the result filled in.
+        ``format`` can be one of 'html', 'json', or 'text' (TODO: Add XML???).
 
-        ``service`` `Service`
-            A back end service object.
-
-        ``template`` `str`
-            Name of template to render.
-
-        ``format``
-            Format of response content: html (default), fragment, or json.
-
-        ``http_status`` `int`
-            HTTP status code.
-
-        ``message`` `str`
-            A message describing the result or an error message.
-
-        ``results`` `list`
-            A list of results returned from the back end service. Empty on
-            error.
+        ``wrapped_in`` 
+            Optional template to wrap the response in
 
         return `WSGIResponse`
 
         """
-        template = '/service/%s.myt' % self.template
-        self.fragment = render(template, oResult=self.result)
-        resp = getattr(self, '_get_%s_response' % self.format)()
-        resp.status_code = self.http_status
+        content, mimetype = getattr(self, '_get_%s_response' % self.format)()
+        if wrapped_in:
+            # TODO: wrap content in template
+            resp = None
+        else:
+            resp = Response(content=content, mimetype=mimetype)
+            resp.status_code = self.http_status
         return resp
 
     #----------------------------------------------------------------------
     def _get_text_response(self):
-        text = render('/service/%s.txt.myt' % self.template, 
-                      oResult=self.result)
-        resp = Response(content=text, mimetype='text/plain')
-        return resp
+        """Get plain text"""
+        template = '/service/%s.txt.myt' % self.template
+        text = render(template, oResult=self.result)
+        return text, 'text/plain'
     
     #----------------------------------------------------------------------
     def _get_json_response(self):
+        """Get a JSON string"""
         simple_obj = {
             'type': self.service.name,
             'message': self.message,
             'results': [r.__simplify__() for r in self.results],
-            'fragment': self.fragment,
+            'fragment': self._get_html_response()[0],
         }
         json = simplejson.dumps(simple_obj)
-        resp = Response(content=json, mimetype='text/json')
-        return resp
+        return json, 'text/plain'
 
     #----------------------------------------------------------------------
     def _get_html_response(self):
+        """Get an HTML fragment"""
+        template = '/service/%s.myt' % self.template
+        html = render(template, oResult=self.result)
+        return html, 'text/html'
+        
+    #----------------------------------------------------------------------
+    def _wrap_response_in(self, template):
         region_key = self.service.region.key or 'all'
         
         c.region_key = region_key
