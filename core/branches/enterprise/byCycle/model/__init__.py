f80f07889b11478435f48fc1570c88b9eaba8254
Wyatt <self@wyattbaldwin.com>
2007-02-22 01:48:46 +0000

    Moving toward Active Record style using Elixir.
    * model.__init__
      * import elixir stuff
      * import byCycle.model.db and get DB
      * (connect): Function to connect dynamic metadata to DB's engine
      * (create_all): Function to create all tables; for now, just creates elixir
        Entity tables
      * (Region): An initial elixir class to experiment with

--- core/branches/enterprise/byCycle/model/__init__.py
+++ core/branches/enterprise/byCycle/model/__init__.py
@@ -15,4 +15,35 @@
 byCycle model package.
 
 """
+import elixir
+from elixir import Entity, Unicode
+from elixir import has_field, using_options
 
+from byCycle.model import db
+
+
+# Get the global database handler
+_db = db.DB()
+
+def connect():
+    """Connect the elixir dynamic metadata to database handler's engine.
+
+    Connects in the context of the current thread.
+
+    Note: this doesn't connect to the database; it just wires elixir's
+    dynamic metadata object up to the engine.
+    
+    """
+    elixir.metadata.connect(_db.engine)
+
+def create_all():
+    _db.turnSQLEchoOn()
+    connect()
+    elixir.create_all()
+    _db.turnSQLEchoOff()
+
+
+class Region(Entity):
+    has_field('title', Unicode)
+    using_options(tablename='regions')
+    
