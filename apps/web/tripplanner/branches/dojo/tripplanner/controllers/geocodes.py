ca1673f290dde09e5d0b5e86137456dd64a16c3f
Wyatt <self@wyattbaldwin.com>
2007-02-24 01:20:08 +0000

    * service.py * geocode.py * route.py   * Renamed to services.py, geocodes.py, routes.py for use with RestController
    * services.py
      * Removed _get_response, _get_text_response, _get_json_response,
        _get_html_response, and _wrap_response_in (moved to rest controller
        (rest.py))
    
    * geocodes.py
      * import services controller instead of service controller
      * Subclass ServicesController (plural) instead of ServiceController
      * (show):
        * Renamed to find
        * Removed ``query`` arg (query will passed as CGI param)
        * return _render_response() instead of _get_response()

--- apps/web/tripplanner/branches/dojo/tripplanner/controllers/geocodes.py
+++ apps/web/tripplanner/branches/dojo/tripplanner/controllers/geocodes.py
@@ -1,16 +1,15 @@
 from byCycle.services.geocode import Service, MultipleMatchingAddressesError
+
 from tripplanner.lib.base import *
-from tripplanner.controllers.service import ServiceController
-import urllib
+from tripplanner.controllers.services import ServicesController
 
 
-###########################################################################
-class GeocodeController(ServiceController):
-    """Controller for handling geocode queries."""
+class GeocodesController(ServicesController):
+    """Controller for interfacing with byCycle Geocode service."""
 
-    #----------------------------------------------------------------------
-    def show(self, query, region):
-        c.q = query
+    def find(self, region_id):
+        q = request.params.get('q', None)
+        c.q = q
         try:
             return super(GeocodeController, self).show(
                 query, region, service_class=Service
@@ -23,4 +22,4 @@ class GeocodeController(ServiceController):
             c.classes = 'errors'
         # We'll get here only if there's an unhandled error in the superclass.
         # Otherwise, the superclass will handle rendering directly.
-        return self._get_response()
+        return self._render_response()
