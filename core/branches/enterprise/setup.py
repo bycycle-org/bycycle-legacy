675cb2df512fd0b356eb29a6b0a2752487f4e1d6
Wyatt <self@wyattbaldwin.com>
2007-03-01 06:50:55 +0000

    Updated Elixir to version 0.2.0 and zope.interface to 3.3.0.1

--- core/branches/enterprise/setup.py
+++ core/branches/enterprise/setup.py
@@ -49,7 +49,7 @@ setup(
     install_requires=(
     'psycopg2==2.0.5.1',
     'SQLAlchemy==0.3.5',
-    'Elixir==0.1.0',
-    'zope.interface==3.3.0',
+    'Elixir==0.2.0',
+    'zope.interface==3.3.0.1',
     ),
 )
