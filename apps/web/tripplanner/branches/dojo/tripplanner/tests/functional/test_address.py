9fc3e159d2f053cc131eaad3163164032717c8bd
Wyatt <self@wyattbaldwin.com>
2007-03-04 10:00:27 +0000

    Cleared out tests for removed controllers; pluralized names to correspond with REST controllers

--- apps/web/tripplanner/branches/dojo/tripplanner/tests/functional/test_address.py
+++
@@ -1,6 +0,0 @@
-from tripplanner.tests import *
-
-class TestAddressController(TestController):
-    def test_index(self):
-        response = self.app.get(url_for(controller='address'))
-        # Test response...
--- apps/web/tripplanner/branches/dojo/tripplanner/tests/functional/test_webservice.py
+++
@@ -1,6 +0,0 @@
-from tripplanner.tests import *
-
-class TestWebserviceController(TestController):
-    def test_index(self):
-        response = self.app.get(url_for(controller='webservice'))
-        # Test response...
