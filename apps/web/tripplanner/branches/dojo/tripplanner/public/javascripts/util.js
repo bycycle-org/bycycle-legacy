908a813bfa201ad7c450a507ab680d29a13578ba
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2007-03-06 05:52:03 +0000

    javascripts.util.appendScript -- Append script element to body instead of head

--- apps/web/tripplanner/branches/dojo/tripplanner/public/javascripts/util.js
+++ apps/web/tripplanner/branches/dojo/tripplanner/public/javascripts/util.js
@@ -11,7 +11,7 @@ byCycle.util = {
     var script = document.createElement('script');
     script.type = type || 'text/javascript';
     script.src = src;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    document.body.appendChild(script);
   },
   
   /** String Functions **/
