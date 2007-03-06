adf0de39ace712c83dac7532519e0bc1f2d5d64d
Wyatt <self@wyattbaldwin.com>
2007-03-06 05:50:17 +0000

    Removed dropping and creating of DB tables; we want to do that as part of the back end set up only

--- apps/web/tripplanner/branches/dojo/tripplanner/websetup.py
+++ apps/web/tripplanner/branches/dojo/tripplanner/websetup.py
@@ -5,7 +5,6 @@ from byCycle import model
 def setup_config(command, filename, section, vars):
     """Set up the application."""
     print '== Setting up byCycle Trip Planner Web application...'
-    
     # Get configuration
     conf = paste.deploy.appconfig('config:' + filename)
     app_conf = conf.local_conf
@@ -13,11 +12,4 @@ def setup_config(command, filename, section, vars):
         'app_conf': app_conf,
         'global_conf': conf.global_conf
     })
-
-    # Create database tables that don't exist
-    print '== Creating database tables (only those that do not exist)...'
-    #model.drop_all()
-    model.create_all()
-    print '== Done creating database tables.'
-
     print '== Done setting up byCycle Trip Planner Web application.'
