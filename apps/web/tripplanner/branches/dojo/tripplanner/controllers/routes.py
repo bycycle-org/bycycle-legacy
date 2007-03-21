e1085c827c5800960dd553e54a791755fc65700b
Wyatt <self@wyattbaldwin.com>
2007-03-21 04:45:01 +0000

    \ Updated to new REST-style, using geocodes.py as a model

--- apps/web/tripplanner/branches/dojo/tripplanner/controllers/routes.py
+++ apps/web/tripplanner/branches/dojo/tripplanner/controllers/routes.py
@@ -1,34 +1,37 @@
 from byCycle.services.route import Service, MultipleMatchingAddressesError
-from byCycle.model.geocode import Geocode
 
 from tripplanner.lib.base import *
-from tripplanner.controllers.service import ServiceController
+from tripplanner.controllers.services import ServicesController
 
 
-class RouteController(ServiceController):
+class RoutesController(ServicesController):
     """Controller for handling route queries."""
 
     #----------------------------------------------------------------------
-    def show(self, query, region):
-        query = self._makeRouteList(query)
-        c.s, c.e = query[0], query[1]
+    def find(self):
+        q = request.params.get('q', None)
+        if q is not None:
+            q = self._makeRouteList(q)
+        else:
+            s = request.params.get('s', None)
+            e = request.params.get('e', None)
+            q = [s, e]
+        c.s, c.e = q[0], q[1]
         c.q = '%s to %s' % (c.s, c.e)
         params = {}
         for p in ('pref', 'tmode'):
             if p in request.params:
                 params[p] = request.params[p]
         try:
-            return super(RouteController, self).show(
-                query, region, service_class=Service, **params
+            return super(RoutesController, self)._find(
+                q, service_class=Service, **params
             )
         except MultipleMatchingAddressesError, exc:
-            template = 'route_geocodes'
-            results = exc.choices
-            http_status = 300
-            c.title = 'Multiple Matches Found'
-            c.classes = 'errors'
+            template = '300'
+            c.http_status = 300
+            self.collection = exc.choices
         # We'll get here only if there's an unhandled error in the superclass.
-        # Otherwise, the superclass will handle rendering directly.
-        return self._get_response(
-            self.service, template, http_status, self.format, results=results
+        # Otherwise, the superclass will handle rendering.
+        return self._render_response(
+            format=self.format, template=template, code=c.http_status
         )
