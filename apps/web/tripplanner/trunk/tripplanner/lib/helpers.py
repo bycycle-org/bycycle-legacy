9a3856fc9aa91fb577ef2fc3dc053f03c6afb3f5
Wyatt <self@wyattbaldwin.com>
2007-04-28 16:46:44 +0000

    Use fixed-pane widget everywhere. There were several places where the fixed-pane markup was hardcode instead of %calling the widget %def.
    * trunk/tripplanner/lib/helpers.py
      * (hide_element): Added ``as_attr`` kwarg--wrap style in style="" if True
    
    * trunk/tripplanner/public/javascripts/widgets/fixed_pane.js
      * (_create_controls): The control_bar is now part of the widget markup, so
        we don't need to ``createElement`` it here
    
    * trunk/tripplanner/public/stylesheets/fixed-pane.css
      * float title left and control-bar right instead of absolutely positioning
        the control-bar (hopefully this will fix the IE7 issue where title bars
        are "disembodied" from their fixed panes when scrolling)
    
    * trunk/tripplanner/templates/layout.html
      * include fixed-pane widget %namespace
      * %call fixed-pane widget for #message_pane
      * %call fixed-pane widget for #ads
    
    * trunk/tripplanner/templates/routes/show.html
      * include fixed-pane widget %namespace
      * %call fixed-pane widget for route results
    
    * trunk/tripplanner/templates/widgets/fixed-pane.html
      * Added ``classes`` keyword arg (list of CSS classes to apply to the top
        level widget node)
      * Use caller.body() instead of caller.content() in .content-pane
    
    * trunk/tripplanner/templates/geocodes/show.html
      * include fixed-pane widget %namespace
      * %call fixed-pane widget for geocode results

--- apps/web/tripplanner/trunk/tripplanner/lib/helpers.py
+++ apps/web/tripplanner/trunk/tripplanner/lib/helpers.py
@@ -13,9 +13,12 @@ def if_ie(content, join_string=''):
     return join_string.join(('<!--[if IE]>', content, '<![endif]-->'))
 
 
-def hide_element(cond=True):
+def hide_element(cond=True, as_attr=True):
     if cond:
-        return 'style="display: none;"'
+        if as_attr:
+            return 'style="display: none;"'
+        else:
+            return 'display: none;'
     else:
         return ''
 
