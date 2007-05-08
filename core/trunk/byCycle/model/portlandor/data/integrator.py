49827cde628a64dafbfd34046075542ce8e54a2c
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2007-05-08 23:46:42 +0000

    Second pass at creating class-based data integration scripts. * Moved script part of portlandor/data/shp2pgsql.py to new script file   scripts/integrate.py, which serves as a front end for integrating any   region * Moved the Integrator class from portlandor/data/shp2pgsql.py to   the new model/data/integrator.py file * The new base Integrator class is still portlandor-specific but less   so than before due to dynamic importing of regional data and entities

---
+++ core/trunk/byCycle/model/portlandor/data/integrator.py
@@ -0,0 +1,20 @@
+###############################################################################
+# $Id: shp2pgsql.py 187 2006-08-16 01:26:11Z bycycle $
+# Created 2006-09-07
+#
+# Portland, OR, data integrator.
+#
+# Copyright (C) 2006, 2007 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>.
+# All rights reserved.
+#
+# For terms of use and warranty details, please see the LICENSE file included
+# in the top level of this distribution. This software is provided AS IS with
+# NO WARRANTY OF ANY KIND.
+###############################################################################
+from byCycle.model.data import integrator
+
+
+class Integrator(integrator.Integrator):
+
+    def __init__(self, *args, **kwargs):
+        super(Integrator, self).__init__(*args, **kwargs)
