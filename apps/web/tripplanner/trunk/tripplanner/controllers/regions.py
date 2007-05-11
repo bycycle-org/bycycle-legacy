9e9303d850b7c51d5c75a1df6f3fb6f612d2403f
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2007-05-11 01:23:55 +0000

    Handle region-not-specified of unknown region more gracefully in regions controller. Save error message in session and display after redirect instead of showing a stark white screen.

--- apps/web/tripplanner/trunk/tripplanner/controllers/regions.py
+++ apps/web/tripplanner/trunk/tripplanner/controllers/regions.py
@@ -9,15 +9,18 @@ class RegionsController(RestController):
         c.service = 'services'
 
     def index(self):
+        if 'errors' in session:
+            c.title = 'Error'
+            c.errors = session.pop('errors')
+            session.save()
+
         # legacy support
         params = dict(request.params)
         prefix = 'bycycle_'
         for k in params:
             if k.startswith(prefix):
                 params[k.lstrip(prefix)] = params.pop(k)
-        id = self._get_region_id(params.pop('region', ''), params)
-        # KLUDGE: _get_region_id shouldn't return "all" by default
-        id = None if id == 'all' else id
+        id = self._get_region_id(params.pop('region', None), params)
         if id:
             if 'fr' in params:
                 params['s'] = params.pop('fr')
@@ -42,10 +45,12 @@ class RegionsController(RestController):
             region_id = self._get_region_id(region_id)
         q = params.get('q', '').strip()
         params.pop('commit', '')
-        if not region_id:
-            self.action = 'index'
+        if region_id is None:
+            session['errors'] = 'Please select a region'
+            session.save()
             self.q = q
-            return self._render_response(template='index')
+            params.pop('q', None)
+            return redirect_to('regions', **params)
         elif q:
             redirect_to('find_services', region_id=region_id, **params)
         else:
@@ -61,7 +66,8 @@ class RegionsController(RestController):
             try:
                 return regions.getRegionKey(region_id)
             except ValueError:
-                c.errors = 'Unknown region: %s' % region_id
+                session['errors'] = 'Unknown region: %s' % region_id
+                session.save()
                 params = params or dict(request.params)
                 params.pop('region', '')
                 params.pop('bycycle_region', '')
