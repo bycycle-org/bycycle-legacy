1de705bbecd75c6d2d573898ff20d827ecd44ca2
Wyatt <self@wyattbaldwin.com>
2008-06-05 02:35:48 +0000

    Removed fnbmap Rails app

--- apps/web/fnbmap/trunk/app/models/location.rb
+++
@@ -1,7 +0,0 @@
-class Location < ActiveRecord::Base
-  has_many :events
-  validates_presence_of :latitude, :longitude
-  validates_numericality_of :latitude, :longitude
-  #validates_numericality_of :zip
-  #validates_length_of :state, :is => 2
-end
