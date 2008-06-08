21f4c3ee5d991abaffcebe4630c5bbb45772ce10
Wyatt <self@wyattbaldwin.com>
2008-06-08 03:14:35 +0000

    Made google ads for results work by using iframe on AJAX calls and using google ads javascript directly on non-AJAX requests.

---
+++ apps/web/tripplanner/trunk/tripplanner/public/javascripts/result_ads.js
@@ -0,0 +1,8 @@
+google_ad_client = "pub-6971619329897557";
+google_ad_slot = "5293686350";
+google_ad_width = 234;
+google_ad_height = 60;
+if (!debug) {
+  byCycle.util.writeScript('http://pagead2.googlesyndication.com/pagead/show_ads.js');
+}
+
