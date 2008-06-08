cc04a2f6233316e661344c2ff85dd5bafaae33c1
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2008-06-08 02:53:32 +0000

    Added infrastructure for placing google ads with every result. Incidentally updated google ad code for right side ad panel. This all works except when loading non-AJAX results. Don't know why.

--- apps/web/tripplanner/trunk/tripplanner/public/javascripts/ads.js
+++ apps/web/tripplanner/trunk/tripplanner/public/javascripts/ads.js
@@ -1,14 +1,8 @@
-google_ad_client = 'pub-6971619329897557';
+google_ad_client = "pub-6971619329897557";
+google_ad_slot = "0423396081";
 google_ad_width = 120;
 google_ad_height = 600;
-google_ad_format = '120x600_as';
-google_ad_type = 'text_image';
-google_ad_channel = '';
-google_color_border = 'd0d7de';
-google_color_bg = 'FFFFFF';
-google_color_link = '4466DD';
-google_color_text = '000000';
-google_color_url = '4466DD';
 if (!debug) {
   byCycle.util.writeScript('http://pagead2.googlesyndication.com/pagead/show_ads.js');
 }
+
