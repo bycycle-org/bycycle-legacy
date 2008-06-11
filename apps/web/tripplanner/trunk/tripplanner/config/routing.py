9e2f5a542d630fc902b082bac2b9adf060569a92
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2008-06-11 20:36:44 +0000

    Admin of region ads
    Added route for region ads
    Added restler.helpers.* to app helpers
    Added empty Ad REST controller
    Set up pointers (svn:externals) to Restler templates, images, and stylesheets

--- apps/web/tripplanner/trunk/tripplanner/config/routing.py
+++ apps/web/tripplanner/trunk/tripplanner/config/routing.py
@@ -26,8 +26,10 @@ def make_map(global_conf={}, app_conf={}):
     mapper.resource('service', 'services', **options)
     mapper.resource('geocode', 'geocodes', **options)
     mapper.resource('route', 'routes', **options)
+    mapper.resource('ad', 'ads', **options)
 
     # This one can be used to display a template directly
     mapper.connect('*url', controller='template', action='view')
 
     return mapper
+
