31370c6224a44df5cd3d33cc91e2caf4cf390307
Wyatt Baldwin <wyatt.lee.baldwin@gmail.com>
2008-09-19 03:54:40 +0000

    Updated collection method ;find to /find for Routes 1.9.2.

--- apps/web/tripplanner/trunk/tripplanner/tests/functional/test_routes.py
+++ apps/web/tripplanner/trunk/tripplanner/tests/functional/test_routes.py
@@ -4,7 +4,7 @@ class TestRouteController(TestController):
     def test_find_member(self):
         url = url_for('routes', region_id='portlandor', action='find',
                       q='633 n alberta to 3950 ne 15th ave, portland')
-        assert url == ('/regions/portlandor/routes;find?'
+        assert url == ('/regions/portlandor/routes/find?'
                        'q=633+n+alberta+to+3950+ne+15th+ave%2C+portland')
         response = self.app.get(url)
         assert response.status == response.c.http_status == 200
